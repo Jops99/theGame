7f8afeee6378cfc342145cd893caf2ac6ded238b
joaopedrossoares <jaopss@outlook.com>
2019-04-19 23:41:57 -0300

    Changing star to hs item, adding first dumb npc and adding first song

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@
 	var game = new Phaser.Game(800,600,Phaser.AUTO,null,{preload:preload,create:create,update:update});
 	var platforms,player,keys,stars,globSndStar,txtScore,score = 0;
 	var keyW,keyA,keyD;
-	
+	var sprite;	
 	
 	function preload(){
 		game.load.image('sky','img/sky.png');
 		game.load.image('diamond','img/diamond.png');
 		game.load.image('platform','img/platform.png');
-		game.load.image('star','img/star.png');
+		game.load.image('star','img/hs.png');
+		game.load.spritesheet('ms', 'img/metalslug_mummy37x45.png', 37, 45, 18);
 		
 		//Carrega o arquivo de áudio
-		game.load.audio('sndStar','audio/sndStar.mp3');
+		game.load.audio('sndStar','audio/Scyphe-Goldrunner_(Maccie_Pimp_Me Up_Remix).mp3');
 		
 		game.load.spritesheet('dude','img/laizaSprite.png',40,48,15);
 	}
@@ -19,6 +20,7 @@
 	function create(){
 		//Associa o áudio à sua variável
 		globSndStar = game.add.audio('sndStar');
+		globSndStar.play();
 	
 		keys = game.input.keyboard.createCursorKeys();
 		
@@ -45,13 +47,23 @@
 		stars = game.add.group();
 		stars.enableBody = true;
 		
+		
+		
 		for(var i = 0; i < 12; i++){
 			var star = stars.create(i*70,0,'star');
-				star.body.gravity.y = 300;
-				star.body.bounce.y = 0.7 + (Math.random()*0.2);
+			star.body.gravity.y = 300;
+			star.body.bounce.y = 0.7 + (Math.random()*0.2);
 		}
-			
-		player = game.add.sprite(50,game.world.height - 150,'dude');
+		// NPC's 
+		sprite = game.add.sprite(50, game.world.height - 110, 'ms');
+		game.physics.arcade.enable(sprite);		
+		sprite.animations.add('walk');sprite
+		sprite.animations.play('walk', 50, true);
+		sprite.body.collideWorldBounds = true;	
+		game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
+		
+		//Player
+		player = game.add.sprite(50, game.world.height - 150,'dude');
 		game.physics.arcade.enable(player);
 		player.body.gravity.y = 300;
 		player.body.bounce.y = 0.2;
@@ -67,6 +79,10 @@
 		game.physics.arcade.collide(stars,platforms);
 		game.physics.arcade.overlap(player,stars,collectStar);
 		
+		/*
+			Beging Player updates
+		*/
+
 		player.body.velocity.x = 0;
 		if(keys.left.isDown || keyA.isDown){
 			player.body.velocity.x = -150;
@@ -81,13 +97,16 @@
 		}
 		
 		if((keys.up.isDown || keyW.isDown) && player.body.touching.down){
-			player.body.velocity.y = -350;
+			player.body.velocity.y = -300;
 		}
+
+		/* 
+			End Player updates 
+		*/
 	}
 	
 	function collectStar(player,star){
 		//Toca o som
-		globSndStar.play();
 		
 		star.kill();
 		score += 10;
